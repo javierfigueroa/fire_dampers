31637f3d667a3647a295eac904f4be0b1441ac06
Javier Figueroa <figueroa7@gmail.com>
2012-04-12 11:51:46 -0400

    adding check for active or inactive accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,11 @@ class User < ActiveRecord::Base
   def role?(base_role)
     ROLES.index(base_role.to_s) <= ROLES.index(role)
   end
+  
+   def active_for_authentication?
+    #remember to call the super
+    #then put our own check to determine "active" state using 
+    #our own "is_active" column
+    super and self.active?
+  end
 end
