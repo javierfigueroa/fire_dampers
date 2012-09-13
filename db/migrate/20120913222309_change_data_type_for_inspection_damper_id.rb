c07322d8e9a4371500d954fb34f9409acd2673ec
Javier Figueroa <figueroa7@gmail.com>
2012-09-13 18:50:16 -0400

    update

--- db/migrate/20120913222309_change_data_type_for_inspection_damper_id.rb
+++ db/migrate/20120913222309_change_data_type_for_inspection_damper_id.rb
@@ -1,8 +1,10 @@
 class ChangeDataTypeForInspectionDamperId < ActiveRecord::Migration
   def up
-     change_table :inspections do |t|
-      t.change :damper_id, :integer
-    end
+     rename_column :inspections, :damper_id, :old_damper_id
+    add_column :inspections, :damper_id, :integer
+   Inspection.reset_column_information
+   Inspection.all.each {|e| e.update_attribute(:damper_id, e.old_damper_id.to_i) }
+   remove_column :inspections, :old_damper_id
   end
 
   def down
