d6fddb10aae396d758623a199652f4e606514372
Javier Figueroa <figueroa7@gmail.com>
2013-06-13 00:13:45 -0400

    added capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+require 'capistrano/ext/multistage'
+
+set :application, "fire_dampers"
+set :scm, :git
+# set :scm_command, "/opt/local/bin/git" 
+set :repository, "git@bitbucket.org:javierfigueroa/fire_dampers.git"
+set :scm_passphrase, "thegrassisgreen"
+set :user, "admin"
+set :default_environment, {'PATH' =>"/opt/local/bin:/opt/local/sbin:/usr/local/sbin:/usr/local/bin:/usr/local/opt/ruby/bin:/usr/local/mysql/bin:/opt/local/lib/postgresql83/bin:/bin:/sbin:/usr/bin:/usr/sbin"}
+set :stages, ["production"]
+set :default_stage, "production"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+server "173.9.149.217", :app, :web, :db, :primary => true
+set :deploy_to, "/Users/admin/Dampers"
