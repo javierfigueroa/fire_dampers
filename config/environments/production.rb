e3862b203a0de59fa2abb43df14bd1f132105aa6
Javier Figueroa <figueroa7@gmail.com>
2013-12-16 23:42:19 -0500

    adding osx intel wkpdf binary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ FireDampers::Application.configure do
     WickedPdf.config = {
       #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
       #:layout => "pdf.html",
-      :exe_path =>  Rails.root.join('bin', 'wkhtmltopdf-OS-X.ppc').to_s
+      :exe_path =>  Rails.root.join('bin', 'wkhtmltopdf-0.9.9-OS-X.i368').to_s
     }
   end
 end
