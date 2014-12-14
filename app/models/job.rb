0580facf20587d20ca71b8bd08cc3b8abca6a2fc
Javier Figueroa <figueroa7@gmail.com>
2014-12-14 12:55:13 -0500

    fixes cascade delete for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,7 @@
 class Job < ActiveRecord::Base
+  has_many :inspection, :dependent => :delete_all
+  has_many :report, :dependent => :delete_all
+  
   validates :name,  :presence => true
   validates :address, :presence => true
   validates :finish_date, :presence => true
